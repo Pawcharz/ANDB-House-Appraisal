1922e39abfbe3dbb3d35f8fb1e2fc963444ba3ad
polemaster <kuba.czermanski@protonmail.com>
2025-01-06 12:55:25 +0100

    SCRUM-78 Integrate ML model with back-end

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from fastapi import Depends, FastAPI
 from pydantic import BaseModel, Field
-# import pickle
+import tensorflow as tf
+import numpy as np
 
 
-app = FastAPI()
+
+# Creating and loading ML model
+
+model = tf.keras.Sequential(
+  [
+    tf.keras.layers.Input([3]),
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dense(64, activation="relu"),
+    tf.keras.layers.Dense(32, activation="relu"),
+    tf.keras.layers.Dense(1),
+  ]
+)
 
 # Load the AI model
-# with open("model.pkl", "rb") as f:
-#     model = pickle.load(f)
-
+MODEL_PATH = "model.h5"
+try:
+    model.load_weights(MODEL_PATH, skip_mismatch=False)
+except Exception as e:
+    raise RuntimeError(f"Failed to load model: {e}")
 
 class InputData(BaseModel):
     bedrooms: int = Field(..., gt=0, lt=400, description="Number of bedrooms must be greater than 0 and less than 400")
@@ -16,12 +30,18 @@ class InputData(BaseModel):
     house_size: int = Field(..., gt=0, description="House size must be greater than 0")
 
 
+app = FastAPI()
 
 @app.get("/predict")
 async def predict(input_data: InputData = Depends()):
-    # print(input_data.model_dump())
-    # Convert input data to a list for the model
-    # features = [[input_data.feature1, input_data.feature2]]  # Add more as needed
-    # prediction = model.predict(features)
-    # return {"predicted_price": prediction[0]}
-    return {"predicted_price": 100000}
+    features = np.array([[input_data.bedrooms, input_data.bathrooms, input_data.house_size]])
+
+    print(f"Features for prediction: {features}")
+    # Make prediction
+    prediction = model.predict(features)  # Predict using the model
+
+    # Extract the predicted value (assuming a regression task)
+    predicted_price = prediction[0][0]
+
+    return {"predicted_price": round(float(predicted_price))}
+
